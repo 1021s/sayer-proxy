d62cfd5ac39fb3534031fd717022e04aeabf1b2d
Sayer Tindall <sayer.tindall@gmail.com>
2019-12-13 10:26:11 -0800

    working proxy

--- server/index.js
+++ server/index.js
@@ -1,21 +1,34 @@
 const express = require('express');
 const request = require('request');
-const app = express();
-const port = 3000;
 
-app.use(express.static(`${__dirname}/../public/dist`));
-app.use(express.urlencoded({ extended: true }));
+const path = require('path');
+
+const app = express();
+const PORT = 3000 || process.env.PORT;
+
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/kyle-service/bundle.js', (req, res) => {
+  request('http://54.213.38.65:3001/main.bundle.js', (err, response) => {
+    res.send(response.body);
+  });
+});
 
-// docker run -d -p 3003:3003 -- sayer
-// docker run -d -p 3005:3005 -- inna
-// docker run -d -p 3001:3001 -- kyle
-// docker run -d -p 3002:3002 -- patrick
-app.get('/sayer-service/bundle.js', (req, res) => {
-  request('http://18.221.25.146:3003/bundle.js', (err, response) => {
+app.get('/kyle-service/vendors.js', (req, res) => {
+  request('http://54.213.38.65:3001/vendors.bundle.js', (err, response) => {
+    res.send(response.body);
+  });
+});
+
+app.get('/api/listings/:Listing_id', (req, res) => {
+  request('http://54.213.38.65:3001/api/listings/' + req.params.Listing_id, (err, response) => {
+    res.send(response.body);
+  });
+});
+
+app.get('/patrick-service/bundle.js', (req, res) => {
+  request('http://54.213.192.23:3002/bundle.js', (err, response) => {
     if (err) {
       console.log(err);
     }
@@ -23,11 +36,41 @@ app.get('/sayer-service/bundle.js', (req, res) => {
   });
 });
 
-app.get('/api/listing/:id/', (req, res) => {
-  request('http://18.221.25.146:3003/api/listing/' + req.params.id, (err, response) => {
+app.get('/listings/:id', (req, res) => {
+  request('http://54.213.192.23:3002/listings/' + req.params.id, (err, response) => {
     if (err) {
       console.log(err);
     }
+    res.send(response.body);
+  });
+});
+
+app.get('/sayer-service/bundle.js', (req, res) => {
+  request('http://18.191.248.186:3003/bundle.js', (err, response) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(response.body);
+  });
+});
+
+app.get('/api/listing/:Listing_id/', (req, res) => {
+  request('http://18.191.248.186:3003/api/listing/' + req.params.Listing_id, (err, response) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(response.body);
+  });
+});
+
+app.get('/matt-service/bundle.js', (req, res) => {
+  request('http://18.237.82.132:3004/mc.bundle.js', (err, response) => {
+    res.send(response.body);
+  });
+});
+
+app.get('/api/houseprices/:listing_id/', (req, res) => {
+  request('http://18.237.82.132:3004/api/houseprices/' + req.params.listing_id, (err, response) => {
     res.send(response.body);
   });
 });
@@ -35,6 +78,7 @@ app.get('/api/listing/:id/', (req, res) => {
 app.get('/inna-service/bundle.js', (req, res) => {
   request('http://35.164.99.110:3005/dist/bundle.js', (err, response) => {
     // console.log('response ', response.body);
+
     if (err) {
       console.log(err);
     }
@@ -47,38 +91,11 @@ app.get('/api/photos/:id/', (req, res) => {
     if (err) {
       console.log(err);
     }
+
     res.send(response.body);
   });
 });
 
-app.get('/patrick-service/bundle.js', (req, res) => {
-  request('http://18.236.158.99:3002/bundle.js', (err, response) => {
-    if (err) {
-      console.log(err);
-    }
-    res.send(response.body);
-  });
-});
-
-app.get('/listings/:id', (req, res) => {
-  request('http://18.236.158.99:3002/listings/' + req.params.id, (err, response) => {
-    if (err) {
-      console.log(err);
-    }
-    res.send(response.body);
-  });
-});
-
-app.get('/kyle-service/bundle.js', (req, res) => {
-  request('http://34.213.221.210:3001/bundle.js', (err, response) => {
-    if (err) console.error(err);
-    res.send(response.body);
-  });
-});
-
-app.get('/api/listings/:id', (req, res) => {
-  request('http://34.213.221.210:3001/api/listings/' + req.params.id, (err, response) => {
-    if (err) console.error(err);
-    res.send(response.body);
-  });
+app.listen(PORT, () => {
+  console.log('Server listening on port: ', PORT);
 });
